4a749777dd69243426befafafb31ae0faefad984
Jonatas Fabricio da Silva <joncleanmgr@gmail.com>
2022-09-20 12:09:28 -0300

    Implements bookNotFoundTest and its codes

--- src/main/java/com/example/libraryapi/api/resource/BookController.java
+++ src/main/java/com/example/libraryapi/api/resource/BookController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
 
@@ -36,8 +37,11 @@ public class BookController {
     @GetMapping("{id}")
     //@ResponseStatus(HttpStatus.ACCEPTED)
     public BookDTO get(@PathVariable Long id){
-        Book book = service.getById(id).get();
-        return modelMapper.map(book, BookDTO.class);
+        return service
+                .getById(id)
+                .map(book -> modelMapper.map(book, BookDTO.class))
+                .orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
--- src/test/java/com/example/libraryapi/api/resource/BookControllerTest.java
+++ src/test/java/com/example/libraryapi/api/resource/BookControllerTest.java
@@ -147,6 +147,21 @@ public class BookControllerTest {
 
     }
 
+    @Test
+    @DisplayName("Deve retornar resource not found quando o livro procurado nao exitir")
+    public void bookNotFoundTest() throws Exception{
+
+        BDDMockito.given(service.getById(Mockito.anyLong())).willReturn(Optional.empty());
+
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+                .get(BOOK_API.concat("/" + 1))
+                .accept(MediaType.APPLICATION_JSON);
+
+        mvc.perform(request)
+                .andExpect(status().isNotFound());
+
+    }
+
     private static BookDTO createNewBook() {
         return BookDTO
                 .builder()
