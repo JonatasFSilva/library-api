5c43e5454f93fc7daafb7a87ebaef4f369b10cf3
Jonatas Fabricio da Silva <joncleanmgr@gmail.com>
2022-09-27 14:33:06 -0300

    .

---
+++ src/main/java/com/example/libraryapi/api/resource/LoanController.java
@@ -0,0 +1,5 @@
+package com.example.libraryapi.api.resource;
+
+public class LoanController {
+
+}
--- src/test/java/com/example/libraryapi/api/resource/LoanControllerTest.java
+++ src/test/java/com/example/libraryapi/api/resource/LoanControllerTest.java
@@ -1,14 +1,29 @@
 package com.example.libraryapi.api.resource;
 
+import com.example.libraryapi.api.dto.LoanDTO;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
 
 @ExtendWith(SpringExtension.class)
 @ActiveProfiles("test")
-@WebMvcTest(controllers = LoanControllerTest.class)
+@WebMvcTest(controllers = LoanController.class)
 @AutoConfigureMockMvc
 public class LoanControllerTest {
+    @Autowired
+    MockMvc mvc;
+
+    @Test
+    @DisplayName("Deve realizar um emprestimo")
+    public void createLoanTest() throws Exception{
+        LoanDTO dto;
+        String json = new ObjectMapper().writeValueAsString(dto);
+    }
 }
